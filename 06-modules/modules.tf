a507fc761db50e37f5a2d2fc146a9b0a24b1e4c4
Sampath Kumar <sampathkm09@gmail.com>
2023-03-12 14:11:54 +0800

    Terraform

--- 06-modules/modules.tf
+++ 06-modules/modules.tf
@@ -9,7 +9,7 @@ module "ec2" {
 
 terraform {
   backend "s3" {
-    bucket = "terraform-b70"
+    bucket = "learnbuckets3"
     key    = "06-modules/terraform.tfstate"
     region = "us-east-1"
   }
